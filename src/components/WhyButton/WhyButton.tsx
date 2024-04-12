fa530fc0b19c54e16fa26c82443cddacc11420cd
Diomedes Git <diomedesgit@gmail.com>
2024-04-13 00:07:46 +0100

    not sure if it's slow or broken

--- src/components/WhyButton/WhyButton.tsx
+++ src/components/WhyButton/WhyButton.tsx
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ollama from 'ollama/browser';
 
 console.log(ollama);
 
 interface WhyButtonProps{
   setResponseText: (responseText: string) => void;
+  latestResponse: string;
 }
 
-const WhyButton: React.FC<WhyButtonProps>= ({ setResponseText }) => {
+const WhyButton: React.FC<WhyButtonProps>= ({ setResponseText, latestResponse }) => {
   const [count, setCount] = useState(0);
-  
+  const [prompt, setPrompt] = useState('Tell me a crazy fact, something that i might not know.');
+
+  useEffect(() => {
+    setResponseText(latestResponse);
+  }, [ latestResponse, setResponseText ]);
+
+   
   const handleClick = async () => {
     const response = await ollama.chat({
       model: 'llama2',
-      messages: [{ role: 'user', content: 'Why is the sky blue?' }],
+      messages: [{ role: 'user', content: prompt }],
 });
 
     setResponseText(response.message.content);
+    setPrompt('okay, so i understand that' + response.message.content + ' But why?')
     setCount(count + 1);
 
 };
